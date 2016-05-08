061de7db5cc72a5ccb97f3aa5c5ffe7161ae4a43
Samantha Diamond <samanthadiamond89@gmail.com>
2016-05-08 19:36:37 +1000

    frustration followed by victory - ongoing issues with scroll functionality, created bubble chart

--- NoiseSpace_web_app/app/assets/javascripts/scatter_plot.js
+++ NoiseSpace_web_app/app/assets/javascripts/scatter_plot.js
@@ -1,16 +1,14 @@
+// declare global variables
+var dataset;
+var minX;
+var maxX;
+var graphTimeWidth = 12*1000*60*60; // 12 hours in milliseconds
+
 var margin = 50;
 var width = 1000;
 var height = 450;
 
-centreDate = # d3 datetime object
-minX = # centre date - 6 hours
-maxX = # centre date + 6 hours
-
 function draw(dataset) {
-  var margin = 50;
-  var width = 1000;
-  var height = 450;
-
   d3.select("body")
     .append("svg")
       .attr("width", width)
@@ -20,7 +18,7 @@ function draw(dataset) {
     .enter()
     .append("circle");
 
-    var x_extent = [minX, maxX];
+  var x_extent = [minX, maxX];
   // var x_extent = d3.extent(dataset, function(d){
   //   return d.datetime;
   // });
@@ -50,13 +48,13 @@ function draw(dataset) {
       .attr("transform", "translate(0," + (height-margin) + ")")
     .call(x_axis);
 
-  var y_axis = d3.svg.axis().scale(y_scale).orient("left");
-
-  d3.select("svg")
-    .append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + margin + ", 0 )")
-    .call(y_axis);
+  // var y_axis = d3.svg.axis().scale(y_scale).orient("left");
+  //
+  // d3.select("svg")
+  //   .append("g")
+  //     .attr("class", "y axis")
+  //     .attr("transform", "translate(" + margin + ", 0 )")
+  //   .call(y_axis);
 
   d3.select(".x.axis")
     .append("text")
@@ -75,13 +73,11 @@ function draw(dataset) {
 
 function shiftgraph(direction) {
   if (direction === "right") {
-    centreDate = # shift centreDate left or right
-    minX = # centre date - 6 hours
-    maxX = # centre date + 6 hours
+    minX.setTime(minX.getTime()+200000);
+    maxX.setTime(maxX.getTime()+200000);
   } else {
-    centreDate = # shift centreDate left or right
-    minX = # centre date - 6 hours
-    maxX = # centre date + 6 hours
+    minX.setTime(minX.getTime()-200000);
+    maxX.setTime(maxX.getTime()-200000);
   }
 
   var x_extent = [minX, maxX];
@@ -102,14 +98,13 @@ function shiftgraph(direction) {
     .attr("cx", function(d){return x_scale(d.datetime);})
     .attr("cy", function(d){return y_scale(d.dB);})
     .attr("r", 4);
+
+  var x_axis = d3.svg.axis().scale(x_scale);
+
+  d3.select('svg').selectAll("g.x.axis")
+        .call(x_axis);
+
 }
-
-
-
-
-
-
-var dataset;
 
 $.ajax({
   type: 'GET',
@@ -120,6 +115,9 @@ $.ajax({
     dataset = response.map(function(d) {
       return {datetime: parseDatetime(d.datetime), dB: +d["dB"]}
     });
+    minX = dataset[0].datetime;
+    maxX = dataset[1].datetime;
+    maxX.setTime(minX.getTime()+graphTimeWidth);
   }
 });
 
