ef82d3a807e1f50cab6b6296d3b791a328fbd8ae
Samantha Diamond <samanthadiamond89@gmail.com>
2016-05-08 22:14:55 +1000

    researched and appended health info. Issues with displaying info

--- NoiseSpace_web_app/app/assets/javascripts/page_intro.js
+++ NoiseSpace_web_app/app/assets/javascripts/page_intro.js
@@ -1,36 +1,36 @@
-// var drawCellsInterval;
-//
-// function refreshWhitenoise() {
-//   drawCellsInterval = setInterval(drawCells, 100);
-// }
-//
-// function drawCells() {
-//   var ctx = document.getElementById('canvasCells').getContext('2d');
-//   for (var y = 0; y <= 200; y++) {
-//     for (var x = 0; x <= 200; x++) {
-//       var cellColor = Math.random() < 0.5 ? "white" : "black";
-//       ctx.fillStyle = cellColor;
-//       ctx.fillRect(x, y, 10, 10);
-//       }
-//     }
-//   }
-//
-// function fadeOutWhitenoiseFadeInHeading() {
-//   $("#canvasCells").delay(3000).fadeOut(5000, function() {
-//     clearInterval(drawCellsInterval);
-//     $("#canvasCells").remove();
-//     fadeInHeading();
-//   });
-// }
-//
-// function fadeInHeading() {
-//   $('body').append('<h1 class="NoiseSpace-title">NoiseSpace.</h1>');
-// }
+var drawCellsInterval;
 
+function refreshWhitenoise() {
+  drawCellsInterval = setInterval(drawCells, 100);
+}
 
-// noise pollution health impact:
-// hearing impairment, hypertension, ischemic heart disease, stress, annoyance, and sleep disturbance.
-// noise exposure increases stress levels leading to physiological arousal, such as raised heart rate and blood pressure.
+function drawCells() {
+  var ctx = document.getElementById('canvasCells').getContext('2d');
+  for (var y = 0; y <= 200; y++) {
+    for (var x = 0; x <= 200; x++) {
+      var cellColor = Math.random() < 0.5 ? "white" : "black";
+      ctx.fillStyle = cellColor;
+      ctx.fillRect(x, y, 10, 10);
+      }
+    }
+  }
 
+function fadeOutWhitenoiseFadeInHeading() {
+  $("#canvasCells").delay(3000).fadeOut(5000, function() {
+    clearInterval(drawCellsInterval);
+    $("#canvasCells").remove();
+    fadeInHeading();
+  });
+}
 
-// 85 dB is roughly equivalent to the noise of heavy truck traffic on a busy road
+function fadeInHeading() {
+  $('body').append('<h1 class="NoiseSpace-title">NoiseSpace.</h1>').fadeIn(3000).delay(3000).fadeOut(3000, function() {
+    healthInfo();
+  });
+}
+
+function healthInfo() {
+  var healthInfoContent = "Noise pollution can damage your health.\n Stress.\n Annoyance.\n High blood pressure.\n Cardiovascular disease.\n Hearing impairment.\n Sleep disturbance.\n These effects are triggered by long-term daily exposure to noise levels above 65 dB or with acute exposure above 80 to 85 dB as reported by the World Health Organisation (WHO).\n 85 dB is roughly equivalent to heavy traffic on a busy road.\n Is noise impacting your health?";
+  $('body').append('<div class="health-info">'+healthInfoContent+'</div>');
+  // $('.health-info').append('<p>Noise pollution can damage your health.\n Stress.\n Annoyance.\n High blood pressure.\n Cardiovascular disease.\n Hearing impairment.\n Sleep disturbance.\n These effects are triggered by long-term daily exposure to noise levels above 65 dB or with acute exposure above 80 to 85 dB as reported by the World Health Organisation (WHO).\n 85 dB is roughly equivalent to heavy traffic on a busy road.\n Is noise impacting your health?</p>');
+}
