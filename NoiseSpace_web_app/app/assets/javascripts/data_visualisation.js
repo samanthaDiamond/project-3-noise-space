b112a5ffaa63b9dc747c74b1806ac16758055546
Samantha Diamond <samanthadiamond89@gmail.com>
2016-05-08 14:56:04 +1000

    working on scroll functionality with data visualisation, slow progress

--- NoiseSpace_web_app/app/assets/javascripts/data_visualisation.js
+++ NoiseSpace_web_app/app/assets/javascripts/data_visualisation.js
@@ -1,3 +1,11 @@
+var margin = 50;
+var width = 1000;
+var height = 450;
+
+centreDate = # d3 datetime object
+minX = # centre date - 6 hours
+maxX = # centre date + 6 hours
+
 function draw(dataset) {
   var margin = 50;
   var width = 1000;
@@ -12,11 +20,12 @@ function draw(dataset) {
     .enter()
     .append("circle");
 
-  var x_extent = d3.extent(dataset, function(d){
-    return d.datetime;
-  });
+    var x_extent = [minX, maxX];
+  // var x_extent = d3.extent(dataset, function(d){
+  //   return d.datetime;
+  // });
 
-  var x_scale = d3.scale.linear()
+  var x_scale = d3.time.scale()
     .range([margin,width-margin])
     .domain(x_extent);
 
@@ -43,10 +52,11 @@ function draw(dataset) {
 
   var y_axis = d3.svg.axis().scale(y_scale).orient("left");
 
-  d3.select("svg") .append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + margin + ", 0 )")
-  .call(y_axis);
+  d3.select("svg")
+    .append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + margin + ", 0 )")
+    .call(y_axis);
 
   d3.select(".x.axis")
     .append("text")
@@ -61,6 +71,37 @@ function draw(dataset) {
     .text("noise(dB)")
       .attr("transform", "rotate (-90, -43, 0) translate(-280)");
 
+}
+
+function shiftgraph(direction) {
+  if (direction === "right") {
+    centreDate = # shift centreDate left or right
+    minX = # centre date - 6 hours
+    maxX = # centre date + 6 hours
+  } else {
+    centreDate = # shift centreDate left or right
+    minX = # centre date - 6 hours
+    maxX = # centre date + 6 hours
+  }
+
+  var x_extent = [minX, maxX];
+
+  var x_scale = d3.time.scale()
+    .range([margin,width-margin])
+    .domain(x_extent);
+
+  var y_extent = d3.extent(dataset, function(d){
+    return d.dB;
+  });
+
+  var y_scale = d3.scale.linear()
+    .range([height-margin, margin])
+    .domain(y_extent);
+
+  d3.selectAll("circle")
+    .attr("cx", function(d){return x_scale(d.datetime);})
+    .attr("cy", function(d){return y_scale(d.dB);})
+    .attr("r", 4);
 }
 
 
--- NoiseSpace_web_app/app/controllers/measurements_controller.rb
+++ NoiseSpace_web_app/app/controllers/measurements_controller.rb
@@ -6,4 +6,11 @@ class MeasurementsController < ApplicationController
       format.json {render json: @measurements}
     end
   end
+
+  def add_measurement
+    # This is a post action
+    # tessel sends measurement json data to this web app using ajax
+    # add data to measurements database
+    # update average of all hourly average measurements
+  end
 end
